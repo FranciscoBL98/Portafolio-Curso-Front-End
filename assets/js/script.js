005bef5b415f272c7554ee338d3932f96dfe8dc3
FranciscoBL98 <fbignacio.98@gmail.com>
2025-12-15 18:02:31 -0300

    Actualizacion termino

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,52 @@
+/* Para validar los datos del formulario */
 const formulario = document.getElementById("formulario");
-const mensaje = document.getElementById("mensaje");
-
+const respuesta = document.getElementById("respuesta");
 
 formulario.addEventListener("submit", function (event) {
     event.preventDefault();
 
     const nombre = document.getElementById("nombre").value.trim();
-    const correo = document.getElementById("correo").value.trim();
-    const mensaje = document.getElementById("mensaje").value.trim();
-
+    const correo = document.getElementById("email").value.trim();
+    const mensaje = document.getElementById("mensajeTexto").value.trim();
 
     if (nombre === "" || correo === "" || mensaje === "") {
         mostrarMensaje("Todos los campos son obligatorios.", "error");
         return;
     }
+
     mostrarMensaje("¡Formulario enviado correctamente!", "exito");
 });
 
 function mostrarMensaje(texto, tipo) {
-    mensaje.innerText = texto;
+    respuesta.innerText = texto;
 
     if (tipo === "error") {
-        mensaje.style.background = "#ffb3b3";
-        mensaje.style.color = "#a10000";
+        respuesta.style.background = "#ffb3b3";
+        respuesta.style.color = "#a10000";
     } else {
-        mensaje.style.background = "#c6ffb3";
-        mensaje.style.color = "#005c00";
+        respuesta.style.background = "#c6ffb3";
+        respuesta.style.color = "#005c00";
     }
 }
+
+
+/* Para cambiar el fondo a claro o oscuro */
+document.getElementById("temaBtn").addEventListener("click", () => {
+    document.body.classList.toggle("light");
+});
+
+
+
+/* Para cambiar el color del boton */
+const colorBtn = document.getElementById("btn-change");
+let colorCambiado = false;
+
+colorBtn.addEventListener("click", () => {
+    if (!colorCambiado) {
+        colorBtn.style.color = "#00ffcc"; // nuevo color de letra
+        colorCambiado = true;
+    } else {
+        colorBtn.style.color = ""; // vuelve al color original
+        colorCambiado = false;
+    }
+});
